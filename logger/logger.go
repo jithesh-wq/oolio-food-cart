5148af48fa910dd7463a65d94665f81cdd07ee6f
jithesh-wq <jitheshtv4@gmail.com>
2025-06-12 16:29:04 +0530

    updating env

--- logger/logger.go
+++ logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
@@ -10,9 +11,12 @@ import (
 var Log *zap.SugaredLogger
 
 func Init() error {
-	if err := godotenv.Load("/home/hp-jv/interview/oolio-food-cart/cmd/.env"); err != nil {
-		log.Println("No .env file found, relying on system environment variables")
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		if err := godotenv.Load("/home/hp-jv/interview/oolio-food-cart/cmd/.env"); err != nil {
+			log.Println("No .env file found, relying on system environment variables")
+		}
 	}
+
 	logger, err := zap.NewProduction()
 	if err != nil {
 		return err
