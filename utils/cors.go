f5acf4b3e0809cadcf828725de4ab65069eaaec9
jithesh-wq <jitheshtv4@gmail.com>
2025-06-16 11:14:27 +0530

    updating cors filter

--- utils/cors.go
+++ utils/cors.go
@@ -1,14 +1,11 @@
 package utils
 
-import (
-	"log"
-	"net/http"
-)
+import "net/http"
 
 func WithCORS(h http.Handler) http.Handler {
-	log.Println("inside cors filter")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, oolio-auth-key")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
 
 		// Handle preflight request
